f5f666c2b4720c7d891c31cd8fdea0c884304be3
Sandeep Chugh <sandeep.chugh@live.com>
2021-05-24 00:16:04 -0500

    added new script to setup test data and refactored controller

--- Carriers/Carriers.Api/Controllers/CarriersController.cs
+++ Carriers/Carriers.Api/Controllers/CarriersController.cs
@@ -31,40 +31,134 @@ namespace Carriers.Api.Controllers
 
         [HttpGet]
         [Route("{carrierCode}")]
-        public async Task<Carrier> GetByCode(string carrierCode)
+        public async Task<ActionResult<Carrier>> GetByCode(string carrierCode)
         {
             //HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-            return await _carrierService.GetCarrier(carrierCode);
+            _logger.LogDebug($"[CarriersController:GetByCode] CarrierCode: {carrierCode}");
+
+            // input validation
+            if (string.IsNullOrWhiteSpace(carrierCode))
+            {
+                return BadRequest("CarrierCode cannot be null or empty");
+            }
+
+            // service
+            var carrier = await _carrierService.GetCarrier(carrierCode);
+
+            // response
+            if (carrier == null)
+            {
+                _logger.LogDebug($"No carrier found for carrier code:{carrierCode}");
+                return NotFound(carrierCode);
+            }
+
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogDebug($"[CarriersController:GetByCode] Carrier: {JsonConvert.SerializeObject(carrier)}");
+            }
+
+            return Ok(carrier);
         }
+        
         [HttpGet]
-        public async Task<IEnumerable<Carrier>> GetCarriers()
+        public async Task<ActionResult<IEnumerable<Carrier>>> GetCarriers()
         {
-            return await _carrierService.GetCarriers();
-        }
+            //HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+            _logger.LogDebug($"[CarriersController:GetCarriers]");
+
+            // service
+            var carriers = await _carrierService.GetCarriers();
+
+            // response
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogDebug($"[CarriersController:GetByCode] Carrier: {JsonConvert.SerializeObject(carriers)}");
+            }
+
+            return Ok(carriers);      }
 
         [HttpDelete]
         [Route("{carrierCode}")]
-        public async Task<int> DeleteCarrier(string carrierCode)
+        public async Task<ActionResult> DeleteCarrier(string carrierCode)
         {
             //HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-            return await _carrierService.DeleteCarrier(carrierCode);
+            _logger.LogDebug($"[CarriersController:DeleteCarrier] CarrierCode: {carrierCode}");
+
+            // input validation
+            if (string.IsNullOrWhiteSpace(carrierCode))
+            {
+                return BadRequest("CarrierCode cannot be null or empty");
+            }
+
+            // service
+            await _carrierService.DeleteCarrier(carrierCode);
+
+            // response
+            _logger.LogDebug($"Carrier with carrier code:{carrierCode} deleted");
+           
+            return Ok();
         }
 
         [HttpPost]
-        [Route("{jsonCarrier}")]
-        public async Task<int> SaveCarrier(string jsonCarrier)
+        public async Task<ActionResult<Carrier>> SaveCarrier([FromBody] Carrier carrier)
         {
+            // input validation
+            if(carrier == null)
+                return BadRequest("Carrier cannot be null");
 
-            //HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-            return await _carrierService.SaveCarrier(JsonConvert.DeserializeObject<Carrier>(jsonCarrier));
+            if (string.IsNullOrWhiteSpace(carrier.CarrierCode))
+                return BadRequest("CarrierCode cannot be null or empty");
+
+            if (string.IsNullOrWhiteSpace(carrier.CarrierName))
+                return BadRequest("CarrierName cannot be null or empty");
+
+            _logger.LogDebug($"[CarriersController:SaveCarrier] Carrier: {JsonConvert.SerializeObject(carrier)}");
+            
+            // service
+            await _carrierService.SaveCarrier(carrier);
+
+            // response
+            _logger.LogDebug($"Carrier added with carrier id: {carrier.CarrierId}");
+
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogDebug($"[CarriersController:SaveCarrier] New Carrier: {JsonConvert.SerializeObject(carrier)}");
+            }
+
+            return Ok(carrier);
         }
 
         [HttpPut]
-        [Route("{jsonCarrier}")]
-        public async Task<int> UpdateCarrier(string jsonCarrier)
+        [Route("{carrierCode}")]
+        public async Task<ActionResult<Carrier>> UpdateCarrier(string carrierCode, [FromBody] Carrier carrier)
         {
-            //HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-            return await _carrierService.UpdateCarrier(JsonConvert.DeserializeObject<Carrier>(jsonCarrier));
+            // input validation
+            if(carrier == null)
+                return BadRequest("Carrier cannot be null");
+
+            // set carrier code from path to carrier object
+            carrier.CarrierCode = carrierCode;
+            
+            if (string.IsNullOrWhiteSpace(carrier.CarrierCode))
+                return BadRequest("CarrierCode cannot be null or empty");
+
+            if (string.IsNullOrWhiteSpace(carrier.CarrierName))
+                return BadRequest("CarrierName cannot be null or empty");
+
+            _logger.LogDebug($"[CarriersController:UpdateCarrier] Carrier: {JsonConvert.SerializeObject(carrier)}");
+            
+            // service
+            await _carrierService.SaveCarrier(carrier);
+
+            // response
+            _logger.LogDebug($"Carrier with carrier code: {carrier.CarrierCode} updated");
+
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogDebug($"[CarriersController:UpdateCarrier] Carrier updated : {JsonConvert.SerializeObject(carrier)}");
+            }
+
+            return Ok(carrier);
         }
     }
 }
